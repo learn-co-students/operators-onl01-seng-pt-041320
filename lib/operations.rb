833aa4a19d40384ed26d03950a962aae3a1e12a1
Patricia Cordero <patricia@patriciaco.com>
2020-04-14 06:03:31 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,22 @@
+require 'pry'
+
+
+
 def unsafe?(speed)
-
+    if speed < 40 || speed > 60
+      true
+    else 
+      false
+  end
 end
-
 
 
 def not_safe?(speed)
-	
+  #binding.pry
+  speed.between?(40,60) ? false : true
 end
-	
+
+
+
 
 
