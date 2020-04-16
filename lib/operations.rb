98c59b2e9dd2862ea4fdc98b17c8474c446c0968
Ifeoluwa  Akinremi-Wade <ifeoluwa.wade@gmail.com>
2020-04-16 04:57:36 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,24 @@
+# require "pry"
+
+# def unsafe?(speed)
+
+  
+
+# end
+
+# def not_safe?(speed)
+	
+# end
 def unsafe?(speed)
+  if speed > 60 || speed < 40
+     true
+  else 
+   false
+  end
 
 end
-
-
 
 def not_safe?(speed)
-	
+	speed > 60 || speed < 40 ? true : false
 end
-	
-
 
