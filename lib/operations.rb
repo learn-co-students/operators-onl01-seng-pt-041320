6781e62d12779556f7befea77d3f215bf230a52c
modern-course-2099 <saraamiry24@gmail.com>
2020-04-20 08:39:33 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,14 @@
 def unsafe?(speed)
+  if speed < 40 || speed > 60
+    true
+  else
+    true && false
+  end
 
 end
 
-
+puts unsafe?(30)
 
 def not_safe?(speed)
-	
+ speed < 40 || speed > 60 ? true : false
 end
-	
-
-
